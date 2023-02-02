5e8c76a7caa64c3d6225d79bca018e616cb75b91
Nehasharma97549 <107554308+Nehasharma97549@users.noreply.github.com>
2023-02-02 21:21:52 +0530

    Add files via upload

---
+++ bootcamp_assignment_34/34_1st.cpp
@@ -0,0 +1,18 @@
+//1-
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    ofstream fout;
+    fout.open("D:/My Life/hello.txt");
+    if (fout)
+    {
+        cout << "File Open..";
+        fout << "Hello World";
+    }
+    else
+        cout << "File not opend...";
+    fout.close();
+    return 0;
+}
